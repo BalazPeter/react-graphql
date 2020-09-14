64f14d2768e093bcc990a411862340ba5b8a974e
sayashchaudhary <sayashchaudhary@gmail.com>
2020-09-14 12:48:19 +0530

    rendering books data

--- src/componensts/BookList.js
+++ src/componensts/BookList.js
@@ -1,13 +1,39 @@
 import React from "react";
+import { gql } from "apollo-boost";
+import { graphql } from "react-apollo";
 
-const BookList = () => {
+const getBooksQuery = gql`
+    {
+        books {
+            name
+            id
+        }
+    }
+`
+
+const BookList = (props) => {
+
+    const displayBooks = () => {
+        let data = props.data
+        if (data.loading) {
+            return (<div>Loading books....</div>)
+        } else {
+            return data.books.map((book) => {
+                return (
+                    <li key={book.id}>{book.name}</li>
+                )
+            })
+        }
+    }
+
     return (
         <div>
             <ul id="book-list">
-                <li>Book Name</li>
+                {displayBooks()}
             </ul>
         </div>
     )
 }
 
-export default BookList;
+export default graphql(getBooksQuery)(BookList);
+
